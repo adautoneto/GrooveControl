bb64b79584e8f7b39b966b068426a4a108a8442e
Adauto <adauto.neto@netmovies.com.br>
2012-05-24 01:53:17 -0300

    Fix: Clicks not working sometimes

--- content_script.js
+++ content_script.js
@@ -1,4 +1,6 @@
-var NowPlaying = function() {
+var NowPlaying = (function() {
+  var controls = $("#player_controls_playback");
+  var oldState = null;
   var current = $(".queue-item-active .queueSong");
 
   function $current(selector) {
@@ -12,23 +14,36 @@ var NowPlaying = function() {
   }
 
   return {
-    isEmptyQueue: $(".queue-item").length == 0,
-    song: $current(".queueSong_name").text(),
-    artist: $current(".queueSong_artist").text(),
-    album: biggerImage( $current(".albumart img").attr("src") ),
-    controls: $("#player_controls_playback").html()
-  }
-}
+    getContent: function() {
+      var playback = {
+        isEmptyQueue: $(".queue-item").length == 0,
+        song: $current(".queueSong_name").text(),
+        artist: $current(".queueSong_artist").text(),
+        album: biggerImage( $current(".albumart img").attr("src") ),
+      };
+
+      var controlsAreTheSame = oldState != null && controls.get(0).isEqualNode(oldState.get(0));
+      if (!controlsAreTheSame) {
+        playback.controls = $("#player_controls_playback").html();
+        oldState = controls.clone();
+      }
+
+      return playback;
+    }
+  };
+});
+
+var nowPlaying = new NowPlaying();
 
 var port = chrome.extension.connect();
 port.onMessage.addListener(function(msg){
   switch(msg.action) {
     case "clickControl":
       $("#" + msg.controlId).click();
-      port.postMessage( {action: "renderPopup", data: new NowPlaying() } );
+      port.postMessage( { action: "renderPopup", data: nowPlaying.getContent() } );
       break;
     case "getContent":
-      port.postMessage( {action: "renderPopup", data: new NowPlaying() } );
+      port.postMessage( { action: "renderPopup", data: nowPlaying.getContent() } );
       break;
   }
 });
